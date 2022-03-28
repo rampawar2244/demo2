dd487cf184b617c72ab8aaf2c8fbde55bad5e34c
Ram pawar <merampawar4@gmail.com>
2022-03-28 17:14:37 +0530

    first

---
+++ app.js
@@ -0,0 +1,18 @@
+//if user add the notes, add it is to the local storage
+
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener('click' ,function(e){
+
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes)
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value ="";
+    console.log(notesObj)
+});
